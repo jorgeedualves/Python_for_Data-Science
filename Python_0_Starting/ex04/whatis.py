e1f55e0a923b22598a51e559d479fda483f7c9f1
jorgeedualves <jorgeedualves84@gmail.com>
2024-09-27 14:22:59 -0300

    ex04 ok

---
+++ Python_0_Starting/ex04/whatis.py
@@ -0,0 +1,19 @@
+import sys
+
+def main():
+    try:
+
+        assert len(sys.argv) <= 2, "more than one argument is provided"
+
+        if(int(sys.argv[1]) % 2 == 0):
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+    except ValueError as e:
+        print("Argument is not a valid integer.")
+    
+if __name__ == "__main__":
+    main()
